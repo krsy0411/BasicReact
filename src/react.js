d7f31dbc49f0acccb71685447ccf89b075433577
krsy0411 <krsy0411@naver.com>
2022-08-23 00:27:42 +0900

    클래스 컴포넌트 이용

--- src/react.js
+++ src/react.js
@@ -1,4 +1,11 @@
 
+// 클래스 컴포넌트
+export class Component {
+  constructor(props) {
+    this.props = props;
+  }
+}
+
 // DOM형성
 export function createDOM(node) {
   // 노드가 text인 경우의 방어코드 작성
@@ -21,15 +28,24 @@ export function createDOM(node) {
   return element;
 }
 
+
+// 클래스 컴포넌트 이용시 속성을 만들어줄 헬퍼함수
+function makeProps(props, children) {
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children,
+  };
+}
 // 전개 연산자를 통해서 children은 배열 형태로 만들어준다
 export function createElement(tag, props, ...children) {
   // babel transpiling을 이용하면 빈 객체 사용시 null로 반환하기 때문에 방어코드 작성
   props = props || {};
 
+/*  // 함수 컴포넌트인지 확인
   if (typeof tag === 'function') {
     // 자식요소가 하나라도 존재하면
     if (children.length > 0) {
-      // 객체 형태로 리턴
+      // 객체 형태로 리턴 : app.js에서 쉽게 props를 받기 위함
       return tag({
         ...props,
         children: children.length === 1 ? children[0] : children,
@@ -39,6 +55,26 @@ export function createElement(tag, props, ...children) {
     }
   } else {
     return { tag, props, children }; 
+  } */
+
+  // 클래스 컴포넌트 이용한 ver
+  if (typeof tag === 'function') {
+    // 일반 typeof 연산자는 클래스와 함수를 구분하지 못하므로 prototype 이용
+    if (tag.prototype instanceof Component) {
+      // 클래스의 인스턴스 객체를 만들어 렌더링
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
+    } else {
+      // 함수 컴포넌트이면서 클래스 컴포넌트는 아닌 경우
+      if (children.length > 0) {
+        return tag(makeProps(props, children))
+      } else {
+        return tag(props);
+      }
+    }
+  } else {
+    // 함수 or 클래스 컴포넌트도 아닌 경우
+    return { tag, props, children };
   }
 }
 
